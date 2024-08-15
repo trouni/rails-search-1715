c01a8fa158e4f4f78a205dcefe9f5ede3d929977
Trouni Tiet <trouni@gmail.com>
2024-08-15 11:01:05 +0900

    Add PgSearch implementation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,19 @@
 class Movie < ApplicationRecord
   belongs_to :director
 
+  # PgSearch Implementation
+  include PgSearch::Model
+  multisearchable against: [:title, :synopsis]
+
+  pg_search_scope :search, # This creates a Movie.search class method (aka scope)
+    against: [:title, :synopsis],
+    associated_against: {
+      director: [:first_name, :last_name]
+    },
+    using: { tsearch: { prefix: true } }
+
   # SQL Implementation
-  def self.search(query)
+  def self.sql_search(query)
     sql_query = <<~SQL
       title @@ :q OR
       synopsis @@ :q OR
